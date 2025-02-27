a9617e790cecc613093cbdb0a53dd76f774f6809
Maksick <mezhow@ya.ru>
2025-02-28 01:11:23 +0500

    🚑 Project: вернул project(ранее был назван task)

---
+++ lib/feature/project/domain/repository/project_repository.dart
@@ -0,0 +1,11 @@
+import 'package:kaban_frontend/feature/project/domain/entity/project_entity.dart';
+
+abstract interface class ProjectRepository {
+  Future<List<Project>> getAllProjects();
+  Future<Project> getProjectById(String id);
+  Future<Project> createProject(Project project);
+  Future<Project> updateProject(String id, Project project);
+  Future<void> deleteProject(String id);
+  Future<Project> addMember(String projectId, String userId);
+  Future<Project> removeMember(String projectId, String userId);
+}
