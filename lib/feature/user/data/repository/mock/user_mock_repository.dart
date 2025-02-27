6e4fcc7761a8a3144cd256dff24f5ecefc855c69
Maksick <mezhow@ya.ru>
2025-02-27 18:23:55 +0500

    добавил метод updateUser, getMe, getUser, getUsersByUsername

--- lib/feature/user/data/repository/mock/user_mock_repository.dart
+++ lib/feature/user/data/repository/mock/user_mock_repository.dart
@@ -1,19 +1,39 @@
-import 'package:kaban_frontend/feature/user/data/model/user_model.dart';
-import 'package:kaban_frontend/feature/user/domain/entity/user_entity.dart';
+import 'package:kaban_frontend/feature/user/data/model/user_mock_model.dart';
 import 'package:kaban_frontend/feature/user/domain/repository/user_repository.dart';
 
 class UserMockRepository implements UserRepository {
+  UserMockModel _currentUser = UserMockModel.mock();
+
   @override
-  Future<User> getUser() async {
-    final User user = UserAPIModel(
-      id: '1',
-      email: 'test@mail.ru',
-      login: 'test_login',
-      username: 'test_user',
-      bio: '@test',
+  Future<UserMockModel> getUser(String id) async {
+    return _currentUser;
+  }
+
+  @override
+  Future<UserMockModel> updateUser(
+    String? email,
+    String? username,
+    String? bio,
+    String? avatar,
+  ) async {
+    _currentUser = _currentUser.copyWith(
+      email: email,
+      username: 'new_name',
+      bio: 'its new bio',
       avatar: '',
-      projectsIds: ['1'],
     );
-    return user;
+
+    return _currentUser;
+  }
+
+  @override
+  Future<UserMockModel> getMe() async {
+    return _currentUser;
+  }
+
+  @override
+  Future<List<UserMockModel>> getUsersByUsername() {
+    // TODO: implement getUsersByUsername
+    throw UnimplementedError();
   }
 }
