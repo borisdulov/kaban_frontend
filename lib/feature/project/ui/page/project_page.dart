1ea623317c03055f0a9e1a035a0b48f97f199c43
Maksick <mezhow@ya.ru>
2025-03-09 22:15:31 +0500

    💄 project_page: исправил appBar

--- lib/feature/project/ui/page/project_page.dart
+++ lib/feature/project/ui/page/project_page.dart
@@ -14,26 +14,103 @@ class ProjectPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 150,
-        leading: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: AppThemeSize.p24,
-            vertical: AppThemeSize.p12,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(64),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.2), width: 1),
+            ),
           ),
-          child: Text('Project Name', style: GoogleFonts.inter(
-            fontSize: 24,
-            fontWeight: FontWeight.w400
-          )),
-        ),
-        title: SmoothCard(
-          borderRadius: BorderRadius.circular(AppThemeRadius.r8),
           child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Row(children: [Icon(Icons.search), Text('Search...')]),
+            padding: EdgeInsets.symmetric(horizontal: AppThemeSize.p24),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(vertical: AppThemeSize.p12),
+                      child: Text(
+                        'Project Name',
+                        style: GoogleFonts.inter(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w400,
+                          color: Color.fromRGBO(68, 68, 68, 1),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: AppThemeSize.p8),
+                    child: SmoothCard(
+                      borderRadius: BorderRadius.circular(AppThemeRadius.r8),
+                      side: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.2)),
+                      color: Colors.white,
+                      smoothness: 0,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 11,
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(
+                              Icons.search,
+                              color: Color.fromRGBO(0, 0, 0, 0.4),
+                            ),
+
+                            SizedBox(width: 10),
+
+                            Expanded(
+                              child: Text(
+                                'Search...',
+                                style: GoogleFonts.inter(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromRGBO(0, 0, 0, 0.4),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset('assets/png/avatar_group.png'),
+
+                        SizedBox(width: AppThemeSize.p8),
+                        
+                        Text(
+                          '+1',
+                          style: GoogleFonts.roboto(
+                            color: Color.fromRGBO(117, 117, 117, 1),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
-        actions: [Image.asset('assets/png/avatar_group.png')],
       ),
       body: MultiBoardListExample(),
     );
