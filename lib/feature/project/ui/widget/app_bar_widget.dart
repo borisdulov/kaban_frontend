1b2ae07a2e36d742329861e5b808fe61a37924f2
Maksick <mezhow@ya.ru>
2025-03-13 17:01:09 +0500

    💄 app_bar_widget: добавил textfield, убрал expanded, вынес имя проекта

--- lib/feature/project/ui/widget/app_bar_widget.dart
+++ lib/feature/project/ui/widget/app_bar_widget.dart
@@ -3,10 +3,12 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:kaban_frontend/core/theme/domain/cubit/theme_bloc.dart';
 import 'package:kaban_frontend/core/theme/domain/entity/app_theme_size.dart';
 import 'package:kaban_frontend/core/theme/entity/app_theme_radius.dart';
-import 'package:smooth_corner/smooth_corner.dart';
+import 'package:kaban_frontend/feature/project/domain/entity/project_entity.dart';
 
 class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
-  const AppBarWidget({super.key});
+  final Project project;
+
+  const AppBarWidget({super.key, required this.project});
 
   @override
   Size get preferredSize => const Size.fromHeight(64);
@@ -25,78 +27,89 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Expanded(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: AppThemeSize.p12),
-                  child: Text(
-                    'Project Name', //TODO выносить как переменную
-                    style: context.themeData.textTheme.titleSmall,
-                  ),
-                ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: AppThemeSize.p12),
+              child: Text(
+                project.name,
+                style: context.themeData.textTheme.titleLarge,
               ),
             ),
 
-            Expanded(
-              flex: 1,
+            Flexible(
               child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: AppThemeSize.p8),
-                //TODO вместо smoothcard сделать textfield
-                child: SmoothCard(
-                  elevation: 0,
-                  borderRadius: BorderRadius.circular(AppThemeRadius.r8),
-                  side: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.2)),
-                  color: Colors.white,
-                  smoothness: 0,
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 11),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.search, color: Color.fromRGBO(0, 0, 0, 0.4)),
-
-                        SizedBox(width: 10),
-
-                        Expanded(
-                          child: Text(
-                            'Search...',
-                            style: GoogleFonts.inter(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w400,
+                padding: EdgeInsets.symmetric(vertical: AppThemeSize.p8),
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    return Container(
+                      constraints: BoxConstraints(maxWidth: 400),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: 'Search...',
+                          hintStyle: GoogleFonts.inter(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                            color: Color.fromRGBO(0, 0, 0, 0.4),
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search,
+                            color: Color.fromRGBO(0, 0, 0, 0.4),
+                          ),
+                          contentPadding: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 11,
+                          ),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(
+                              AppThemeRadius.r8,
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 0, 0, 0.2),
+                              width: 1.0,
+                            ),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(
+                              AppThemeRadius.r8,
+                            ),
+                            borderSide: BorderSide(
+                              color: Color.fromRGBO(0, 0, 0, 0.2),
+                              width: 1.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(
+                              AppThemeRadius.r8,
+                            ),
+                            borderSide: BorderSide(
                               color: Color.fromRGBO(0, 0, 0, 0.4),
+                              width: 1.5,
                             ),
                           ),
                         ),
-                      ],
-                    ),
-                  ),
+                      ),
+                    );
+                  },
                 ),
               ),
             ),
 
-            Expanded(
-              flex: 1,
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    //TODO вынести
-                    Image.asset('assets/png/avatar_group.png'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                //TODO вынести
+                Image.asset('assets/png/avatar_group.png'),
 
-                    SizedBox(width: AppThemeSize.p8),
+                SizedBox(width: AppThemeSize.p8),
 
-                    Text(
-                      '+1',
-                      style: GoogleFonts.roboto(
-                        color: Color.fromRGBO(117, 117, 117, 1),
-                      ),
-                    ),
-                  ],
+                Text(
+                  '+1',
+                  style: GoogleFonts.roboto(
+                    color: Color.fromRGBO(117, 117, 117, 1),
+                  ),
                 ),
-              ),
+              ],
             ),
           ],
         ),
