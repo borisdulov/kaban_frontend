610d219730b93c0dffa63d8caad008b1001b21d8
Maksick <mezhow@ya.ru>
2025-03-15 00:29:13 +0500

    🎨 constants: добавил константы для ассетов и переместил радиус и размер

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,12 @@
+class AppAssets {
+  static const String avatarGroup = 'assets/png/avatar_group.png';
+  static const String avatarAccount = 'assets/png/avatar_account.png';
+  static const String sideBar = 'assets/svg/Sidebar.svg';
+  static const String columns = 'assets/svg/columns.svg';
+  static const String checkSquare = 'assets/svg/check_square.svg';
+  static const String briefcase = 'assets/svg/briefcase.svg';
+  static const String settings = 'assets/svg/settings.svg';
+  static const String clock = 'assets/svg/clock.svg';
+  static const String barChart = 'assets/svg/bar_chart.svg';
+  static const String list = 'assets/svg/list.svg';
+}
--- lib/core/constants/app_radius.dart
+++ lib/core/constants/app_radius.dart
@@ -1,4 +1,4 @@
-class AppThemeRadius {
+class AppRadius {
   static const double r4 = 4.0;
   static const double r8 = 8.0;
   static const double r16 = 16.0;
